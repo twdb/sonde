e510c6816281a0900e5c5ff812c87b1a56bf458b
Andy Wilson <andrew.wilson@twdb.state.tx.us>
2010-12-29 16:57:19 -0600

    Adding unit tests for the BaseSondeDataset object

--- docs/api.rst
+++ docs/api.rst
@@ -5,15 +5,28 @@ API
 
 .. module:: sonde
 
-This part of the documentation covers all the interfaces of sonde.
+
+The easiest way to read a data file is through its format interface::
+
+    from sonde import Sonde
+    dataset = Sonde('path/to/ysi_file.dat', 'ysi', param_def='path/to/ysi_param.def')
+
+    dataset.dates   # contains a numpy array of datetime objects
+    dataset.data    # contains a dict with numpy arrays for each data field mapped to the field name
+
+
 
 
 
 Sonde Object
 ------------
 
-.. autoclass:: Sonde
+.. autoclass:: sonde.formats.ysi.Dataset
    :members:
    :inherited-members:
 
 
+.. autoclass:: Sonde
+   :members:
+   :inherited-members:
+   
--- tests/sonde_tests.py
+++ tests/sonde_tests.py
@@ -15,6 +15,7 @@ from datetime import datetime
 import os
 from nose.tools import assert_almost_equal, set_trace
 import numpy as np
+import quantities as pq
 import re
 import sys
 import unittest
@@ -24,12 +25,68 @@ import unittest
 #sonde_path = os.path.join(os.path.dirname(__file__), '..')
 #sys.path.append(os.path.join(sonde_path))
 
-from sonde import Sonde
+
+from sonde import BaseSondeDataset, Sonde
+from sonde import quantities as sq
 from sonde.timezones import cdt, cst
 from sonde.formats import ysi
 
 
 ysi_test_files_path = os.path.join(os.path.dirname(__file__), 'ysi_test_files')
+
+
+class SondeTestDataset(BaseSondeDataset):
+    """
+    A test dataset
+    """
+
+    def __init__(self):
+        super(SondeTestDataset, self).__init__()
+
+    def _read_data(self):
+        date_str_list = ['2010-12-23 11:00:24',
+                     '2010-12-23 12:00:24',
+                     '2010-12-23 13:00:24',
+                     '2010-12-23 14:00:24',
+                     '2010-12-23 15:00:24',
+                     '2010-12-23 16:00:24',]
+
+        date_fmt = '%Y-%m-%d %H:%M:%S'
+
+        date_list = []
+        for date_str in date_str_list:
+            date = datetime.strptime(date_str, date_fmt)
+            date = date.replace(tzinfo=cdt)
+            date_list.append(date)
+
+        self.dates = np.array(date_list)
+
+        self.data = {
+            'BAT01': np.array([ 6.2,  6.2,  6.2, 5.6,  5.6,  5.6]) * pq.volt,
+            'CON02': np.array([-0.   , -0.   ,  8.326,  0.782,  1.964,  0.174]) * sq.mScm,
+            'DOX02': np.array([  95.8  ,  101.767,   82.465, 102.95 ,  102.094,  109.647]) * pq.percent,
+            'TEM01': np.array([ 23.45,  25.24,  21.34, 25.7 ,  26.  ,  20.38]) * pq.degC,
+            'WSE01': np.array([ 1, 1, 1, 1, 1, 1]) * pq.m,
+            }
+
+        self.parameters = {
+            'TEM01' : ['Water Temperature', pq.degC],
+            'CON02' : ['Conductivity(Not Normalized)', sq.mScm],
+            'WSE01' : ['Water Surface Elevation (No Atm Pressure Correction)', pq.m],
+            'BAT01' : ['Battery Voltage', pq.volt],
+            'DOX02' : ['Dissolved Oxygen Saturation Concentration', pq.percent],
+            }
+
+                 
+class BaseSondeDatasetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_dataset = SondeTestDataset()
+
+    def test_set_and_get_standard_unit(self):
+        self.test_dataset.set_standard_unit('WSE01', pq.m)
+        unit = self.test_dataset.get_standard_unit('WSE01')
+
+
 
 
 def ysi_csv_read(filename):
