6fa92fe523f77b8e581ea84c6bc916da222869a5
Andy Wilson <andrew.wilson@twdb.state.tx. <Andy Wilson <andrew.wilson@twdb.state.tx.>
2011-03-16 19:52:28 -0500

    add open_sonde to sonde __init__.py

--- sonde/__init__.py
+++ sonde/__init__.py
@@ -4,9 +4,10 @@
 
     A utility for reading in water data from a variety of device
     formats
-    
+
 """
 
-from .sonde import BaseSondeDataset, master_parameter_list, Sonde, autodetect, merge, find_tz, default_static_timezone
+from .sonde import autodetect, BaseSondeDataset, default_static_timezone, \
+     find_tz, master_parameter_list, merge, open_sonde, Sonde
 from . import quantities
 from . import formats
