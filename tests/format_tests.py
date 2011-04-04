aafcc0d776856dfb29d37e9ab95b4973504ccc01
Andy Wilson <wilson.andrew.j@gmail.com>
2011-04-04 16:32:27 -0500

    add tests for sonde.autodetect plus some general cleanup

--- tests/format_tests.py
+++ tests/format_tests.py
@@ -7,7 +7,7 @@ from nose.tools import assert_almost_equal, eq_, set_trace
 import numpy as np
 import quantities as pq
 
-from sonde import Sonde
+import sonde
 from sonde import quantities as sq
 from sonde.timezones import cst, cdt
 
@@ -27,10 +27,20 @@ def test_files():
 
         test_file_config = ConfigObj(open(test_config_path), unrepr=True)
 
+        yield check_autodetect, test_file_config, sonde_file_path
+
         yield check_file, test_file_config, sonde_file_path
 
         with open(sonde_file_path) as sonde_file_fid:
             yield check_file, test_file_config, sonde_file_fid
+
+
+def check_autodetect(test_file, sonde_file):
+    file_format = test_file['header']['format']
+    autodetect_result = sonde.autodetect(sonde_file)
+
+    assert autodetect_result == file_format, \
+           "Autodetection failed: %s != %s" % (autodetect_result, file_format)
 
 
 def check_file(test_file, sonde_file):
@@ -46,23 +56,25 @@ def check_file(test_file, sonde_file):
     else:
         tz = cst
 
-    sonde = Sonde(sonde_file, file_format=file_format, tzinfo=tz)
-    check_format_parameters(test_file['format_parameters'], sonde)
+    sonde_file = sonde.open_sonde(sonde_file, file_format=file_format,
+                                  tzinfo=tz)
+    check_format_parameters(test_file['format_parameters'], sonde_file)
 
     parameters = test_file['data']['parameters']
     units = test_file['data']['units']
 
     for test_data in test_file['data']['test_data']:
-        check_values_match(test_data, parameters, units, sonde)
+        check_values_match(test_data, parameters, units, sonde_file)
 
 
-def check_values_match(test_data, parameters, units, sonde):
+def check_values_match(test_data, parameters, units, sonde_file):
     date = _convert_to_aware_datetime(test_data[0])
 
-    assert date in sonde.dates, "date not found in sonde: %s" % (date)
+    assert date in sonde_file.dates, \
+           "date not found in sonde_file: %s" % (date)
 
     for parameter, unit, test_value in zip(parameters, units, test_data[1:]):
-        sonde_datum = sonde.data[parameter][sonde.dates == date]
+        sonde_datum = sonde_file.data[parameter][sonde_file.dates == date]
 
         if unit in pq.__dict__:
             test_quantity = pq.__dict__[unit]
@@ -87,29 +99,29 @@ def check_values_match(test_data, parameters, units, sonde):
             assert_almost_equal(test_datum, sonde_datum)
 
 
-def check_format_parameters(format_parameters, sonde):
+def check_format_parameters(format_parameters, sonde_file):
     for parameter_name, test_value in format_parameters.items():
         if test_value == '':
             continue
 
-        # parameters on the sonde object itself; historically, these
+        # parameters on the sonde_file object itself; historically, these
         # used to be in the format_parameters dict but now they are on
-        # the sonde object and it's not worth the effort to rearrange
+        # the sonde_file object and it's not worth the effort to rearrange
         # all the test files
         sonde_parameters = ['serial_number', 'site_name', 'setup_time',
                             'start_time', 'stop_time']
 
         if parameter_name in sonde_parameters:
-            assert hasattr(sonde, parameter_name), \
+            assert hasattr(sonde_file, parameter_name), \
                    "format parameter '%s' not found in " \
-                   "sonde.format_parameters" % parameter_name
-            sonde_parameter = getattr(sonde, parameter_name)
+                   "sonde_file.format_parameters" % parameter_name
+            sonde_parameter = getattr(sonde_file, parameter_name)
 
         else:
-            assert parameter_name in sonde.format_parameters, \
+            assert parameter_name in sonde_file.format_parameters, \
                    "format parameter '%s' not found in " \
-                   "sonde.format_parameters" % parameter_name
-            sonde_parameter = sonde.format_parameters[parameter_name]
+                   "sonde_file.format_parameters" % parameter_name
+            sonde_parameter = sonde_file.format_parameters[parameter_name]
 
         # if we are testing a datetime value
         if re.match('\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}', test_value):
